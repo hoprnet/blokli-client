55a4676491dca89c1b639c536d6717c3d911fbcd
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2026-08-04 23:03:09 +0100

    feat: update blokli server image

--- nix/scripts/test-integration.sh
+++ nix/scripts/test-integration.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-export BLOKLI_TEST_REMOTE_IMAGE="${BLOKLI_TEST_REMOTE_IMAGE:-europe-west3-docker.pkg.dev/hoprassociation/docker-images/bloklid-anvil-curvy:0.12.1-commit.4734ba1@sha256:e6a01cc90d275e6f59a2c94b7acf3c51306acee0dbd30feeb7a3228034a8403a}"
+export BLOKLI_TEST_REMOTE_IMAGE="${BLOKLI_TEST_REMOTE_IMAGE:-europe-west3-docker.pkg.dev/hoprassociation/docker-images/bloklid-anvil-curvy:0.12.1-commit.d6a56c1@sha256:52dac3df68893b7df508a7096f212f678bffa1236323ffc038a0d7e3dbb69db0}"
 export BLOKLI_TEST_WORKSPACE_ROOT="${BLOKLI_TEST_WORKSPACE_ROOT:-$PWD}"
 export BLOKLI_TEST_IMAGE="${BLOKLI_TEST_IMAGE:-bloklid-anvil:integration-test}"
 export BLOKLI_TEST_EXTERNAL_STACK=true
